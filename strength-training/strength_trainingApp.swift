14ce97d703079795649bdb60000808ac508f8bfb
danielkuhlwein <danielkuhlwein@me.com>
2026-02-21 11:29:59 -0700

    Initial Commit

---
+++ strength-training/strength_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  strength_trainingApp.swift
+//  strength-training
+//
+//  Created by Daniel Kuhlwein on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct strength_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
